f4926dddb4f6984a796823115fdb440e4354eb50
mounikamummadi97 <mounikamummadi1206@gmail.com>
2022-10-28 10:21:16 +0530

    first commit

---
+++ foreach1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] myArr = { 12, 16, 13, 14, 17 }; //foreach
+
+
+foreach (var item in myArr)
+{
+    if (item % 2 == 0)
+    {
+        Console.WriteLine(item);
+    }
+}
+
+//Console.WriteLine(i);
+//int val;
